6aacc22480e5fa54f225e46e9d1dfcd93dda0753
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-09-10 19:21:39 +0600

    install package library

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Telegraf } = require("telegraf");
-require("dotenv").config();
+// const { Telegraf } = require("telegraf");
+// require("dotenv").config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply(""));
-bot.hears("suli", (ctx) => ctx.reply("Красавчик"));
-bot.hears("ratbek", (ctx) => ctx.reply("Кот"));
-bot.launch();
+// bot.start((ctx) => ctx.reply("Welcome"));
+// bot.help((ctx) => ctx.reply("Send me a sticker"));
+// bot.on("sticker", (ctx) => ctx.reply(""));
+// bot.hears("suli", (ctx) => ctx.reply("Красавчик"));
+// bot.hears("ratbek", (ctx) => ctx.reply("Кот"));
+// bot.launch();
